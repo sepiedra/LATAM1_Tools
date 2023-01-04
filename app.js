a19a300e4395cff2a08d3331efc031b948d98b87
sepiedra <121911936+sepiedra@users.noreply.github.com>
2023-01-03 19:18:02 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let boton = document.getElementById('calcular');
+let respuesta = document.getElementById('respuesta');
+
+let min_LV = 355.2;
+let min_S = 400.2;
+
+let STD_P = 22.95;
+let STD_S = 18.72;
+let STD_C = 14.73;
+let STD_Q = 8.83;
+
+boton.addEventListener('click',calcularTM);
+
+function calcularTM(){
+    let P = parseFloat(document.getElementById('P').value);
+    let S = parseFloat(document.getElementById('S').value);
+    let C = parseFloat(document.getElementById('C').value);
+    let QC = parseFloat(document.getElementById('Q').value);
+    let TM = parseFloat(document.getElementById('TM').value);
+    let t = 0;
+    if (document.getElementById('Y').checked==true) {
+        t = min_S-TM;
+    } else {
+        t = min_LV-TM;
+    }
+    let R = 100*((P*STD_P)+(S*STD_S)+(C*STD_C)+(QC*STD_Q))/t;
+    respuesta.innerHTML = `Su producción VA es: ${R.toFixed(2)} %`;
+}
