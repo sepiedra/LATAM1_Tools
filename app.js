94548bb04957f0fdae7abb60c3915cb271cda4b2
Seb <121911936+sepiedra@users.noreply.github.com>
2023-02-17 20:11:19 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ let respuesta = document.getElementById('respuesta');
 let min_LV = 355.2;
 let min_S = 400.2;
 
-let STD_P = 26.39;
-let STD_S = 19.11;
-let STD_C = 13.46;
+let STD_P = 22.95;
+let STD_S = 18.72;
+let STD_C = 14.73;
 let STD_Q = 8.83;
+
+let P;
+let S;
+let C;
+let QC;
+let TM;
+let t;
 
 boton.addEventListener('click',calcularTM);
 
 function calcularTM(){
-    let P = parseFloat(document.getElementById('P').value);
-    let S = parseFloat(document.getElementById('S').value);
-    let C = parseFloat(document.getElementById('C').value);
-    let QC = parseFloat(document.getElementById('Q').value);
-    let TM = parseFloat(document.getElementById('TM').value);
-    let t = 0;
+    P = parseFloat(document.getElementById('P').value);
+    S = parseFloat(document.getElementById('S').value);
+    C = parseFloat(document.getElementById('C').value);
+    QC = parseFloat(document.getElementById('Q').value);
+    TM = parseFloat(document.getElementById('TM').value);
     if (document.getElementById('Y').checked==true) {
         t = min_S-TM;
     } else {
